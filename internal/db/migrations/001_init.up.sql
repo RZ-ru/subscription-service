ee5b0b311eb459cb6741e882ca93558bccbe3693
Roman Zakruzhnoy <roman.zakruzhnoy@gmail.com>
2025-12-02 13:57:02 +0700

    модифицированы миграции

--- internal/db/migrations/001_init.up.sql
+++ internal/db/migrations/001_init.up.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE INDEX idx_subscriptions_user_id ON subscriptions (user_id);
-CREATE INDEX idx_subscriptions_service_name ON subscriptions (service_name);
-CREATE INDEX idx_subscriptions_start_date ON subscriptions (start_date);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id ON subscriptions (user_id);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_service_name ON subscriptions (service_name);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_start_date ON subscriptions (start_date);
+
